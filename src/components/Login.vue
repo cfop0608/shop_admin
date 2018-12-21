f6d1b134443fd8350724b373a36cda5f0cd326d3
cfop <cfop0608@qq.com>
2018-12-21 09:32:06 +0800

    登录功能完成

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,13 +1,132 @@
 <template>
-  <div class="login">登录</div>
+  <div class="login">
+    <!-- 整个form组件 -->
+    <el-form ref="form" :model="form" :rules="rules" status-icon label-width="80px">
+      <img src="@/assets/avatar.jpg" alt="">
+      <!-- 一个表单项 -->
+      <el-form-item label="用户名" prop="username">
+        <el-input v-model="form.username" placeholder="请输入用户名"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input v-model="form.password" type="password" placeholder="请输入密码"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="login">登录</el-button>
+        <el-button @click="reset">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
-
 <script>
+// 导入axios
+import axios from 'axios'
 export default {
+  data() {
+    return {
+      // 收集表单数据
+      form: {
+        username: '',
+        password: ''
+      },
+      // 表单的校验规则
+      rules: {
+        // 用户名的校验规则
+        username: [
+          { required: true, message: '用户名不能为空', trigger: 'blur' },
+          {
+            min: 3,
+            max: 9,
+            message: '用户名长度在 3 到 9 个字符',
+            trigger: 'change'
+          }
+        ],
+        // 密码校验规则
+        password: [
+          { required: true, message: '密码不能为空', trigger: 'blur' },
+          {
+            min: 6,
+            max: 12,
+            message: '密码长度在 6 到 12 个字符',
+            trigger: 'change'
+          }
+        ]
+      }
+    }
+  },
+  methods: {
+    // 登录
+    login() {
+      this.$refs.form.validate(valid => {
+        if (valid) {
+          // alert('登录成功')
+          // 发送ajax请求
+          axios({
+            url: 'http://localhost:8888/api/private/v1/login',
+            method: 'post',
+            data: this.form
+          }).then(res => {
+            console.log(res.data)
+            if (res.data.meta.status === 200) {
+              // alert('登录成功')
+              this.$message({
+                message: res.data.meta.msg,
+                type: 'success',
+                duration: 1000
+              })
+              // 把后台办法的token存起来
+              localStorage.setItem('token', res.data.data.token)
 
+              // 跳转到Home组件
+              this.$router.push('/home')
+            } else {
+              // 失败的消息
+              this.$message({
+                message: res.data.meta.msg,
+                type: 'error',
+                duration: 1000
+              })
+            }
+          })
+        } else {
+          // console.log('登录失败')
+          return false
+        }
+      })
+    },
+    // 重置
+    reset() {
+      this.$refs.form.resetFields()
+    }
+  }
 }
 </script>
 
-<style>
+<style lang="less">
+.login {
+  // height: 100%;
+  width: 100%;
+  background-color: #2d434c;
+  overflow: hidden;
 
+  .el-form {
+    width: 400px;
+    background-color: #fff;
+    margin: 200px auto;
+    padding: 75px 40px 15px 40px;
+    position: relative;
+
+    img {
+      position: absolute;
+      left: 50%;
+      transform: translateX(-50%);
+      top: -70px;
+      border-radius: 50%;
+      border: 10px solid #fff;
+    }
+
+    .el-button:nth-child(2) {
+      margin-left: 80px;
+    }
+  }
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,46 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/Login'
-
+// 导入Home组件
+import Home from '@/components/Home'
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
       component: Login
+    },
+    {
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/home',
+      component: Home
     }
   ]
 })
+// 给router对象注册一个导航守卫
+// 将来所有的导航（路由发生了跳转）都要经过beforeEach
+// to : 去哪儿
+// from: 从哪儿
+// next : 是否放行  next() 直接放行  next('/login') 强制跳转login页面
+router.beforeEach((to, from, next) => {
+  // console.log('hehe')
+  // console.log(to, from)
+  // 如果要去login页直接放行
+  if (to.path === '/login') {
+    // 直接放行
+    next()
+  } else {
+    // 如果不是login页，判断是否有token，如果有 直接放行，如果没有， 登录页
+    let token = localStorage.getItem('token')
+    if (token) {
+      next()
+    } else {
+      next('/login')
+    }
+  }
+})
+export default router
